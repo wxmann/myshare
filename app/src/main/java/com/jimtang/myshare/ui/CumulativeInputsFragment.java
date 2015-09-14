b8586145bc38f690de43391058be7b8cba440110
wxmann <wxfreakk91@gmail.com>
2015-09-13 18:01:23 -0700

    Break original front-page UI into fragments, one for adding individual purchases and one for adding cumulative amounts

---
+++ app/src/main/java/com/jimtang/myshare/ui/CumulativeInputsFragment.java
@@ -0,0 +1,25 @@
+package com.jimtang.myshare.ui;
+
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jimtang.myshare.R;
+
+/**
+ * Created by tangz on 9/13/2015.
+ */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class CumulativeInputsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.cumul_amts_frag, container, false);
+    }
+}
