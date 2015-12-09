188bf5900dfaa26f014f4fbec5a85d626b115be9
wxmann <wxfreakk91@gmail.com>
2015-12-09 00:57:18 -0800

    Improve formatting of retrieved saved shares.

---
+++ app/src/main/java/com/jimtang/myshare/adapter/HistoricalSharesAdapter.java
@@ -0,0 +1,66 @@
+package com.jimtang.myshare.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.jimtang.myshare.R;
+import com.jimtang.myshare.model.MyShareSession;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by tangz on 12/5/2015.
+ */
+public class HistoricalSharesAdapter extends ArrayAdapter<MyShareSession> {
+
+    public HistoricalSharesAdapter(Context context, int resource, List<MyShareSession> sessions) {
+        super(context, resource, sessions);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View newView = convertView;
+        if (newView == null) {
+            LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            newView = inflater.inflate(R.layout.display_historical_shares, null);
+        }
+
+        MyShareSession session = getItem(position);
+
+        TextView title = (TextView) newView.findViewById(R.id.sessionTitle);
+        title.setText(session.getSessionName());
+
+        TextView time = (TextView) newView.findViewById(R.id.sessionTime);
+        time.setText(new SimpleDateFormat("MM/dd/yyyy h:mm a").format(session.getTimestamp()));
+
+        TextView people = (TextView) newView.findViewById(R.id.sessionPeople);
+        people.setText(peopleString(session));
+
+        return newView;
+    }
+
+    private String peopleString(MyShareSession session) {
+        Collection<String> people = session.getPeople();
+        List<String> orderedPeopleList = Lists.newArrayList(people);
+        Collections.sort(orderedPeopleList);
+
+        int pplCt = orderedPeopleList.size();
+        if (pplCt > 2) {
+            return "Involves: " + Joiner.on(", ").join(orderedPeopleList.get(0), orderedPeopleList.get(1),
+                    String.format("and %s more...", pplCt - 2));
+        } else {
+            return "Involves: " + Joiner.on(", ").join(orderedPeopleList);
+        }
+    }
+}
